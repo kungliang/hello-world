e6aba2f33169011c5168ea3c6456e1584464a278
kl <kl.wang@delta.com.tw>
2016-01-15 17:51:40 +0800

    add testDb

---
+++ testDb.php
@@ -0,0 +1,14 @@
+<?php
+    $dbhost = '127.0.0.1';
+    $dbuser = 'root';
+    $dbpass = 'abcde';
+    $dbname = 'tsms';
+    $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die('Error with MySQL connection');
+    mysql_query("SET NAMES 'utf8'");
+    mysql_select_db($dbname);
+    $sql = "SELECT COUNT(*) as total FROM `ppo_users_list` ;";
+    $result = mysql_query($sql) or die('MySQL query error');
+    while($row = mysql_fetch_array($result)){
+        echo $row['name'];
+    }
+?>
