07a22f1c1ec6d6231cca766658385e0d190f0688
kungliang <kungliang.wang@gmail.com>
2016-01-18 22:15:44 +0800

    Update testDb.php
    test

--- testDb.php
+++ testDb.php
@@ -1,14 +1,14 @@
 <?php
     $dbhost = '127.0.0.1';
     $dbuser = 'root';
-    $dbpass = 'abcde';
-    $dbname = 'tsms';
+    $dbpass = '';
+    $dbname = '';
     $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die('Error with MySQL connection');
     mysql_query("SET NAMES 'utf8'");
     mysql_select_db($dbname);
     $sql = "SELECT COUNT(*) as total FROM `ppo_users_list` ;";
     $result = mysql_query($sql) or die('MySQL query error');
     while($row = mysql_fetch_array($result)){
-        echo $row['name'];
+        echo $row['total'];
     }
 ?>
